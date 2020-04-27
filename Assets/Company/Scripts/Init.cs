870fd4ae049571beb7f37879aa1c322ec1de1f17
Ondrej Slinták <ondrowan@gmail.com>
2020-04-27 21:08:47 +0200

    Initial commit

---
+++ Assets/Company/Scripts/Init.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CloudOnce;
+
+public class Init : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cloud.Initialize(false, true);
+    }
+}
